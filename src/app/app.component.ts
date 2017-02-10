743af75d51bc74e9dd770cb57be2f68d236a7454
willy <reyno.willy@gmail.com>
2017-02-10 16:33:50 +0100

    Working todo list, displayed remaining todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    // Save current typed todo
+    this.todoDataService.addTodo(this.newTodo);
+    // Reset newTodo ?
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    // set chosen todo to true or false
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    // delete a todo
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    // retrieve all todos
+    return this.todoDataService.getAllTodos();
+  }
+
+  get remainingTodos() {
+    return this.todoDataService.getRemainingTodos();
+  }
+
+
 }
--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -1,8 +1,75 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Todo} from './todo';
 
 @Injectable()
 export class TodoDataService {
 
-  constructor() { }
+  /* Placeholder for last id
+  to simulate automatic incrementing id's */
+  lastId: number = 0;
 
+  /* Placeholder for todos */
+  todos: Todo[] = [];
+
+
+  constructor() {
+  }
+
+  // Simulate POST /todos
+  addTodo(todo: Todo): TodoDataService {
+    if (!todo.id) {
+      // auto-increment
+      todo.id = ++this.lastId;
+    }
+    // add new one to todos
+    this.todos.push(todo);
+    return this;
+  }
+
+  // Simulate DELETE /todos/:id
+  deleteTodoById(id: number): TodoDataService {
+    // Reset todos to the same without the id passed as parameter
+    this.todos = this.todos
+      .filter(todo => todo.id !== id);
+    return this;
+  }
+
+  // Simulate PUT /todos/:id
+  updateTodoById(id: number, values: Object = {}): Todo {
+    // Retrieve todo to update
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    // Insert les valeurs données dans l'objet (cible, source)
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  getRemainingTodos(): Todo[] {
+    return this.todos
+      .filter(todo => todo.complete === false)
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number): Todo {
+    // Get todo matching the id
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo) {
+    // Updating complete to reverse boolean
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
 }
